6442d036d49f72237c2ae6867c1bec799dae4d16
qifei.liu <qifei.liu@citrix.com>
2018-07-19 11:17:14 +0800

    1. Create a django project using pycharm 2. Create database using models.py 3. Add static folder with jquery and jquery-cookie

---
+++ news/models.py
@@ -0,0 +1,32 @@
+from __future__ import unicode_literals
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+# Create your models here.
+
+class Coloumn(models.Model):
+    name = models.CharField("栏目名称",max_length=256)
+    slug = models.CharField('栏目地址',max_length=256, db_index= True)
+    intro = models.TextField('栏目简介',default='')
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = '栏目'
+        verbose_name_plural='栏目'
+        ordering = ['name'] # 按照那个栏目排序
+
+class Article(models.Model):
+    column = models.ManyToManyField(Coloumn,verbose_name='归属栏目')
+    Title = models.CharField('标题',max_length=256)
+    slug = models.CharField('网址',max_length=256,db_index=True)
+
+    author = models.ForeignKey('auth.User', blank=True,null=True,verbose_name='作者',on_delete= models.CASCADE)
+    content = models.TextField('内容',default='',blank=True)
+
+    published = models.BooleanField('正式发布',default=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = '教程'
+        verbose_name_plural = '教程'
